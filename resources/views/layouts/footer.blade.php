5634a9b7e7b60acfea9d59a6ab88a6e3f57235fc
thewhitewolf2411 <39027423+thewhitewolf2411@users.noreply.github.com>
2020-05-04 13:42:20 +0200

    Footer changes

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,55 +1,42 @@
 <div class="footer-container">
 
     <div class="footer-column" id="column-one">
+        <div id="column-one-text-container">
+            <p class="footer-small-text" id="column-one-text"> Sva prava zadržana. Nijedan dio ove publikacije ne smije se reprodukovati, prenositi, prepisivati, pohraniti u kakvom pretražnom sistemu niti prevoditi na bilo koji jezik, u bilo kojem obliku ili na bilo koji način, bez pismene dozvole Webinar.ba. </p>
+        </div>
         <div id="footer-logo-container">
             <img id="footer-logo" src="{{ asset('/assets/images/footer/footer-logotip.svg') }}">
+            <img id="footer-logo" src="{{ asset('/assets/images/footer/footer-smartlab-logotip.svg') }}">
         </div>
-
-        <div id="column-one-text-container">
-            <p class="footer-text-small" id="column-one-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-
     </div>
 
     <div class="footer-column" id="column-two">
-        <div class="footer-header-container">
-            <h3 class="footer-header">Lorem Ipsum</h3>
-        </div>
 
-        <p class="footer-small-text">Lorem Ipsum</p>
-        <p class="footer-small-text">Lorem Ipsum</p>
-        <p class="footer-small-text">Lorem Ipsum</p>
-        <p class="footer-small-text">Lorem Ipsum</p>
+        <div class="footer-header-container">
+            <h3 class="footer-header">Kontakt</h3>
+        </div>
+        <p class="footer-small-text"><img class="footer-icons" src="{{ asset('/assets/images/footer/ic_phone_iphone_24px.svg') }}">+387 33 956 222</p>
+        <p class="footer-small-text"><img class="footer-icons" src="{{ asset('/assets/images/footer/ic_mail_24px.svg') }}">info@webinar.ba</p>
+        <p class="footer-small-text"><img class="footer-icons" src="{{ asset('/assets/images/footer/ic_place_24px.svg') }}">Bulevar Meše Selimovića 81A</p>
 
     </div>
 
     <div class="footer-column" id="column-three">
         <div class="footer-header-container">
-            <h3 class="footer-header">Lorem Ipsum</h3>
+            <h3 class="footer-header">Meni</h3>
         </div>
 
-        <p class="footer-small-text">Lorem Ipsum</p>
-        <p class="footer-small-text">Lorem Ipsum</p>
-        <p class="footer-small-text">Lorem Ipsum</p>
-        <p class="footer-small-text">Lorem Ipsum</p>
-
-    </div>
-
-    <div class="footer-column" id="column-four">
-        <div class="footer-header-container">
-            <h3 class="footer-header">Lorem Ipsum</h3>
-        </div>
-
-        <p class="footer-small-text">Lorem Ipsum</p>
-        <p class="footer-small-text">Lorem Ipsum</p>
-        <p class="footer-small-text">Lorem Ipsum</p>
-        <p class="footer-small-text">Lorem Ipsum</p>
+        <a class="footer-link" href="/"><p class="footer-small-text">Naslovna</p></a>
+        <a class="footer-link" href="/#pogodnosti-container"><p class="footer-small-text">Pogodnosti</p></a>
+        <a class="footer-link" href="/#o-nama-container"><p class="footer-small-text">Šta drugi kažu</p></a>
+        <a class="footer-link" href="/#preuzimanje-container"><p class="footer-small-text">Downloads</p></a>
+        <a class="footer-link" href="/#kontakt-container"><p class="footer-small-text">Kontakt</p></a>
 
     </div>
 
     <div class="footer-column" id="column-five">
         <div class="footer-header">
-            <h3 class="footer-header">Follow US</h3>
+            <h3 class="footer-header">Zapratite nas</h3>
         </div>
         <div id="footer-social-icons-container">
             <img class="footer-social-icons" src="{{ asset('/assets/images/footer/facebook-icon.svg') }}">
@@ -61,3 +48,5 @@
     </div>
 
 </div>
+
+<div><p class="rights-class">© <script>document.write( new Date().getFullYear() );</script>  All rights reserved. SmartLab.</p></div>
